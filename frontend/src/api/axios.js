72c23ad954357f9bd0b1e0749aac0d99821dba06
Ameer Hamza <169024920+ameer-hamza26@users.noreply.github.com>
2025-08-10 21:07:07 +0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://vercel.com/ameer-hamza26s-projects/full-stack-attendence-management-system-758m/api',
 });
 
 api.interceptors.request.use((config) => {
