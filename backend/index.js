e8df53fc4abec90ba1739f1b15cefc3452fc79cf
Ameer Hamza <169024920+ameer-hamza26@users.noreply.github.com>
2025-08-11 19:46:31 +0500

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,8 +37,8 @@ app.use(cors({
     return callback(new Error('Not allowed by CORS'));
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 // Explicitly handle preflight
